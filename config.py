cb662fb4b58c573255feb692563b5cd7ef608705
gtapiero <giovanny.tapiero@pi.edu.co>
2025-09-18 11:58:05 -0500

    segundo commit

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+ORACLE_USER = os.getenv('ORACLE_USER', 'system')
+ORACLE_PASSWORD = os.getenv('ORACLE_PASSWORD', 'Tapiero123')
+
+# DSN formato : host:port/service_name -> ejemplo: 127.0.0.1:1521/orcl
+ORACLE_DSN = os.getenv('ORACLE_DSN', 'host:1521/orcl')
+
+#opciones de pool
+POOL_MIN = int(os.getenv('POOL_MIN', '1'))
+POOL_MAX = int(os.getenv('POOL_MAX', '4'))
+POOL_INC = int(os.getenv('POOL_INC', '1'))
